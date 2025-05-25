71e31ee9baa7fe0eaba9a9d54b4f187effa74731
donaldBurbano <thegraymasquerade@gmail.com>
2025-05-25 14:02:17 -0700

    Cleaning up dead weight.

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cp -r web/* /path/to/destination/
+
+# Navigate to the destination directory
+cd /path/to/destination/web
+
+# Install dependencies
+npm install
+
+# Start the application
+npm start
